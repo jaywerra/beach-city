d3228a622e6183e01bff31f9869acf3a03472448
Jay <jaywerra@gmail.com>
2024-08-08 18:01:13 -0700

    removing ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown'
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://beach-city.vercel.app/',
-	output: 'hybrid',
 	integrations: [
 		mdx(), 
 		sitemap(),
